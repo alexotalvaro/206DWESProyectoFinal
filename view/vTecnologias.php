0a107db9aba11d364c84f65786316763b86b88bb
Alejandro <Alejandro@ALEJANDRO-PC>
2023-02-23 09:27:42 +0100

    23/02/2023 phpdoc INTRODUCIDO en la app en la ventana de tecnologias

--- view/vTecnologias.php
+++ view/vTecnologias.php
@@ -8,6 +8,7 @@
 <h4>Tecnologías</h4>
 <h3><a href="doc/tema1.pdf"  target="_blank" style="color: black; text-decoration: none"> TEMA 1: DESARROLLO WEB EN ENTORNO SERVIDOR </a> </h3>
 <h3><a href="doc/tema2.pdf"  target="_blank" style="color: black; text-decoration: none"> TEMA 2: INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ENTORNO DE DESARROLLO Y DEL ENTORNO DE EXPLOTACIÓN </a> </h3>
+<h3><a href="doc/phpdoc/index.html"  target="_blank" style="color: black; text-decoration: none"> PHP DOC</a> </h3>
 <form>
     <input class="botonLogin" type="submit" name="volver" value="Volver">
 </form>
